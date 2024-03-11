d7984129ac98830bddeb6b4e4d78b000dac5dfc9
mshankar0101 <madhushankar0101@gmail.com>
2024-03-12 02:27:21 +0530

    done

--- models/product.js
+++ models/product.js
@@ -2,5 +2,11 @@
 var mongoose= require("mongoose");
 
 //write your schema Here with name of schema as productSchema
+const productSchema = {
+    name:{type:String,required:true},
+    Quantity:{type:Number,required:true},
+    name:{type:Number,required:true},
+    name:{type:String,enum:['Yes','No']}
 
+}
 module.exports =mongoose.model("products",productSchema);
