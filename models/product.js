0d96232b6cad32690e7effffd74ffda786ea347e
Mshankar0101 <144152894+Mshankar0101@users.noreply.github.com>
2024-03-19 23:57:38 +0530

    Update product.js

--- models/product.js
+++ models/product.js
@@ -1,11 +1,52 @@
 
-var mongoose= require("mongoose");
+// var mongoose= require("mongoose");
 
-//write your schema Here with name of schema as productSchema
+// //write your schema Here with name of schema as productSchema
+// const productSchema = new mongoose.Schema({
+//     name:{type:String,required:true},
+//     Quantity:{type:Number,required:true},
+//     Price:{type:Number,required:true},
+//     inStock:{type:String,enum:['Yes','No']}
+// })
+// module.exports =mongoose.model("products",productSchema);
+const mongoose = require('mongoose');
+
 const productSchema = new mongoose.Schema({
-    name:{type:String,required:true},
-    Quantity:{type:Number,required:true},
-    Price:{type:Number,required:true},
-    inStock:{type:String,enum:['Yes','No']}
-})
-module.exports =mongoose.model("products",productSchema);
+
+name: {
+
+type: String,
+
+required: true
+
+},
+
+quantity: {
+
+type: Number,
+
+required: true
+
+},
+
+price: {
+
+type: Number,
+
+required: true
+
+},
+
+instock: {
+
+type: String,
+
+enum: ['Yes', 'No'],
+
+required: true
+
+}
+
+});
+
+module.exports = mongoose.model('Product', productSchema);
